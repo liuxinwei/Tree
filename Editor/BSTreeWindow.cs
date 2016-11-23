34b55c35a48255ff71f9458bc44a6742c03569a4
bruce <liuxinweibruce@163.com>
2016-11-23 21:36:00 +0800

    test

--- Editor/BSTreeWindow.cs
+++ Editor/BSTreeWindow.cs
@@ -6,71 +6,72 @@ using System.Collections.Generic;
 
 public class BSTreeWindow : EditorWindow
 {
-    BSTree root;
+    TreeNode root;
+    int value;
     int maxLayer;
     int delvalue = -1;
     void OnGUI()
     {
         GUILayout.BeginHorizontal();
-        if(GUILayout.Button("生成树"))
-        {
-            List<int> list = new List<int> { 50, 30, 70, 10, 40, 90, 80 , 20 , 22, 23, 24};
-            root = BSTreeBLL.Create(list);
-            maxLayer = 0;
-            BSTreeBLL.ResetIndex(root);
-            BSTreeBLL.ResetLayer(root, ref maxLayer);
-        }
-        if (GUILayout.Button("del"))
-        {
-            if (delvalue != -1)
-            {
-                BSTreeBLL.Delete(ref root, delvalue);
-                if (root != null)
-                {
-                    maxLayer = 0;
-                    root.layer = 1;
-                    root.index = 1;
-                    BSTreeBLL.ResetIndex(root);
-                    BSTreeBLL.ResetLayer(root, ref maxLayer);
-                }
-                delvalue = -1;
-            }
-        }
         GUILayout.EndHorizontal();
 
-        BSTree.wRatio = EditorGUILayout.FloatField("wRatio", BSTree.wRatio);
-        BSTree.hRatio = EditorGUILayout.FloatField("hRatio", BSTree.hRatio);
+        TreeNode.wRatio = EditorGUILayout.FloatField("wRatio", TreeNode.wRatio);
+        TreeNode.hRatio = EditorGUILayout.FloatField("hRatio", TreeNode.hRatio);
+        EditorGUILayout.BeginHorizontal();
+        value = EditorGUILayout.IntField("Value", value);
+        if(GUILayout.Button("Add Node"))
+        {
+            List<int> list = new List<int> { 50, 30, 70, 10, 40, 90, 80, 20, 22, 23, 24 };
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (root == null)
+                {
+                    root = new TreeNode();
+                    root.data = list[i];
+                    root.layer = 1;
+                    root.index = 1;
+                }
+                else
+                {
+                    maxLayer = 0;
+                    BinaryTree.Insert(root, list[i]);
+                    BinaryTree.ResetIndex(root);
+                    BinaryTree.ResetLayer(root, ref maxLayer);
+                }
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        value = EditorGUILayout.IntField("Value", value);
+        if (GUILayout.Button("Del Node"))
+        {
+            BinaryTree.Delete(ref root, value);
+            maxLayer = 0;
+            root.index = 1;
+            root.layer = 1;
+            BinaryTree.ResetIndex(root);
+            BinaryTree.ResetLayer(root, ref maxLayer);
+        }
+        EditorGUILayout.EndHorizontal();
+
         CalculatePosition(root);   
         DrawLine(root);
         DrawNode(root);
-
-        //if (delvalue != -1)
-        //{
-        //    BSTreeBLL.Delete(ref root, delvalue);
-        //    if (root != null)
-        //    {
-        //        maxLayer = 0;
-        //        root.layer = 1;
-        //        root.index = 1;
-        //        BSTreeBLL.ResetIndex(root);
-        //        BSTreeBLL.ResetLayer(root, ref maxLayer);
-        //    }
-        //    delvalue = -1;
-        //}
     }
 
-    void CalculatePosition(BSTree _root)
+    void CalculatePosition(TreeNode _root)
     {
         if(_root == null)
         {
             return;
         }
         _root.CalculatePosition(maxLayer);
-        CalculatePosition(_root.Left);
-        CalculatePosition(_root.Right);
+        CalculatePosition(_root.leftNode);
+        CalculatePosition(_root.rightNode);
     }
 
-    void DrawNode(BSTree _tree)
+    void DrawNode(TreeNode _tree)
     {
         if (_tree == null)
         {
@@ -78,34 +79,34 @@ public class BSTreeWindow : EditorWindow
         }
         Rect rect = new Rect(_tree.point.x - BSTree.nodeSize / 2, _tree.point.y - BSTree.nodeSize / 2, BSTree.nodeSize, BSTree.nodeSize);
 
-        if (delvalue == _tree.Data)
+        if (delvalue == _tree.data)
         {
             GUI.color = Color.red;
         }
-        if(GUI.Button(rect, _tree.Data.ToString()))
+        if (GUI.Button(rect, _tree.data.ToString()))
         {
-            delvalue = _tree.Data;
+            delvalue = _tree.data;
         }
         GUI.color = Color.white;
-        DrawNode(_tree.Left);
-        DrawNode(_tree.Right);
+        DrawNode(_tree.leftNode);
+        DrawNode(_tree.rightNode);
     }
 
-    void DrawLine(BSTree _tree)
+    void DrawLine(TreeNode _tree)
     {
         if (_tree == null)
         {
             return;
         }
-        if(_tree.Left != null)
+        if(_tree.leftNode != null)
         {
-            Handles.DrawLine(_tree.point, _tree.Left.point);
-            DrawLine(_tree.Left);
+            Handles.DrawLine(_tree.point, _tree.leftNode.point);
+            DrawLine(_tree.leftNode);
         }
-        if(_tree.Right != null)
+        if(_tree.rightNode != null)
         {
-            Handles.DrawLine(_tree.point, _tree.Right.point);
-            DrawLine(_tree.Right);
+            Handles.DrawLine(_tree.point, _tree.rightNode.point);
+            DrawLine(_tree.rightNode);
         }
     }
 }
---
+++ Script/BinaryTree/BinaryTree.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinaryTree : MonoBehaviour 
+{
+    public static void Insert(TreeNode _root, int key)
+    {
+        if(_root == null)
+        {
+            return;
+        }
+        if (key > _root.data)
+        {
+            if (_root.rightNode == null)
+            {
+                _root.rightNode = new TreeNode();
+                _root.rightNode.data = key;
+            }
+            else
+            {
+                Insert(_root.rightNode, key);
+            }
+        }
+        else if(key < _root.data)
+        {
+            if (_root.leftNode == null)
+            {
+                _root.leftNode = new TreeNode();
+                _root.leftNode.data = key;
+            }
+            else
+            {
+                Insert(_root.leftNode, key);
+            }
+        }
+    }
+
+    public static TreeNode Search(TreeNode _root, int _key)
+    {
+        if (_root == null)
+        {
+            return null;
+        }
+        if (_root.data == _key)
+        {
+            return _root;
+        }
+        if (_root.data < _key)
+        {
+            return Search(_root.rightNode, _key);
+        }
+        else
+        {
+            return Search(_root.rightNode, _key);
+        }
+    }
+
+    public static void Delete(ref TreeNode _root, int _key)
+    {
+        if (_root == null)
+        {
+            return;
+        }
+        if (_root.data == _key)
+        {
+            if (_root.rightNode == null && _root.leftNode == null)
+            {
+                _root = null;
+            }
+            else if (_root.rightNode == null && _root.leftNode != null)
+            {
+                _root = _root.leftNode;
+            }
+            else if (_root.rightNode != null && _root.leftNode == null)
+            {
+                _root = _root.rightNode;
+            }
+            else
+            {
+                TreeNode parentNode = _root.leftNode;
+                TreeNode curNode = _root.leftNode;
+                while (curNode != null)
+                {
+                    if (curNode.rightNode == null)
+                    {
+                        break;
+                    }
+                    parentNode = curNode;
+                    curNode = curNode.rightNode;
+                }
+                if (parentNode == curNode)
+                {
+                    curNode.rightNode = _root.rightNode;
+                    _root = curNode;
+                }
+                else
+                {
+                    parentNode.rightNode = curNode.leftNode;
+                    curNode.leftNode = _root.leftNode;
+                    curNode.rightNode = _root.rightNode;
+                    _root = curNode;
+                }
+            }
+            return;
+        }
+        if (_root.data < _key)
+        {
+            Delete(ref _root.rightNode, _key);
+        }
+        else
+        {
+            Delete(ref _root.leftNode, _key);
+        }
+    }
+
+    public static void ResetIndex(TreeNode _bsTree)
+    {
+        if (_bsTree == null)
+        {
+            return;
+        }
+        if (_bsTree.leftNode != null)
+        {
+            _bsTree.leftNode.index = _bsTree.index * 2;
+            ResetIndex(_bsTree.leftNode);
+        }
+        if (_bsTree.rightNode != null)
+        {
+            _bsTree.rightNode.index = _bsTree.index * 2 + 1;
+            ResetIndex(_bsTree.rightNode);
+        }
+    }
+
+    public static void ResetLayer(TreeNode _bsTree, ref int _maxLayer)
+    {
+        if (_bsTree == null)
+        {
+            return;
+        }
+        if (_bsTree.leftNode != null)
+        {
+            _bsTree.leftNode.layer = _bsTree.layer + 1;
+            _maxLayer = Mathf.Max(_maxLayer, _bsTree.leftNode.layer);
+            ResetLayer(_bsTree.leftNode, ref _maxLayer);
+        }
+        if (_bsTree.rightNode != null)
+        {
+            _bsTree.rightNode.layer = _bsTree.layer + 1;
+            _maxLayer = Mathf.Max(_maxLayer, _bsTree.rightNode.layer);
+            ResetLayer(_bsTree.rightNode, ref _maxLayer);
+        }
+    }
+
+    public static void DLR(TreeNode _bsTree)
+    {
+        if (_bsTree == null)
+        {
+            return;
+        }
+        DLR(_bsTree.leftNode);
+        DLR(_bsTree.rightNode);
+    }
+}
